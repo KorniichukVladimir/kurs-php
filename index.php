3e38e0a960419bbda46c9271189e614ef9a2dd5f
KorniichukVladimir <vkornijschuk@gmail.com>
2020-01-26 20:13:56 +0200

    масиви

--- index.php
+++ index.php
@@ -7,23 +7,61 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>php</title>
 </head>
+
 <?php
-$h = date('H');
+//$h = date('H');
+//
+//  if ($h <= 6) {
+//    $c = 0;
+//  } elseif ($h > 6 && $h <= 12) {
+//    $c = 1;
+//  } elseif ($h > 12 && $h <= 18) {
+//      $c = 2;
+//  } elseif ($h > 18 && $h <= 24) {
+//      $c = 3;
+//  } else {
+//      $c = 0;
+//  }
+//
+//?>
 
-  if ($h <= 6) {
-    $c = 0;
-  } elseif ($h > 6 && $h <= 12) {
-    $c = 1;
-  } elseif ($h > 12 && $h <= 18) {
-      $c = 2;
-  } elseif ($h > 18 && $h <= 24) {
-      $c = 3;
-  } else {
-      $c = 0;
-  }
+<?php
+//$arr = [4, 5, 7, 8];
+//$arr[4] = 'hello'; // добавить сточку четвертим елементом масива
+//
+//echo '<pre>';
+//print_r($arr);
+//echo '</pre>';
+//
+//?>
 
-?>
-<body style="background-image: url('<?php echo $c; ?>.jpg'); background-size: cover; color: white;">
+<?php
+//$arry = ['Moscow', 'Kiev', 'Borispol', 'Bilorus'];
+//
+//for($i = 0; $i < count($arry); $i++) {
+//    echo $arry[$i] . '<br>';
+//}
+//
+//?>
+
+<?php
+//// асоціативний масив
+//$arr = [
+//    'Ukraine' => 'Kiev',
+//    'Russia' => 'Moscow',
+//    'English' => 'London',
+//    'China' => 'Beijing'
+//];
+//
+////echo $arr['China'];
+//
+//foreach($arr as $key => $value){
+//    echo "$key - $value<br>";
+//}
+//
+//?>
+
+<body>
 
 <h1>php</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur commodi consectetur deleniti, eveniet
