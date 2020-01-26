326c5af09a803a5aa5a0e365c9a7b90a56403f08
KorniichukVladimir <vkornijschuk@gmail.com>
2020-01-26 22:24:01 +0200

    arry

--- index.php
+++ index.php
@@ -8,22 +8,93 @@
     <title>php</title>
 </head>
 <?php
-$h = date('H');
+//$h = date('H');
+//
+//  if ($h <= 6) {
+//    $c = 0;
+//  } elseif ($h > 6 && $h <= 12) {
+//    $c = 1;
+//  } elseif ($h > 12 && $h <= 18) {
+//      $c = 2;
+//  } elseif ($h > 18 && $h <= 24) {
+//      $c = 3;
+//  } else {
+//      $c = 0;
+//  }
+//
+//?>
 
-  if ($h <= 6) {
-    $c = 0;
-  } elseif ($h > 6 && $h <= 12) {
-    $c = 1;
-  } elseif ($h > 12 && $h <= 18) {
-      $c = 2;
-  } elseif ($h > 18 && $h <= 24) {
-      $c = 3;
-  } else {
-      $c = 0;
-  }
+<?php
+//$arr = [4, 5, 7, 8];
+//$arr[4] = 'hello'; // добавить сточку четвертим елементом масива
+//
+//echo '<pre>';
+//print_r($arr);
+//echo '</pre>';
+//
+//?>
+
+<?php
+//$arry = ['Moscow', 'Kiev', 'Borispol', 'Bilorus'];
+//
+//for($i = 0; $i < count($arry); $i++) {
+//    echo $arry[$i] . '<br>';
+//}
+//
+//?>
+
+<?php
+//// асоціативний масив
+//$arr = [
+//    'Ukraine' => 'Kiev',
+//    'Russia' => 'Moscow',
+//    'English' => 'London',
+//    'China' => 'Beijing'
+//];
+//
+////echo $arr['China'];
+//
+//foreach($arr as $key => $value){
+//    echo "$key - $value<br>";
+//}
+//
+//?>
+
+<?php
+//// вложені масиви
+//$arr = [
+//    'Ukraine' => ['Kiev', 'Kharkov', 'Lvov', 'Chernigov'],
+//    'Russia' => ['Moscow', 'Novosebirsk'],
+//    'English' => ['London', 'Marsel', 'Lion'],
+//    'China' => ['Beijing', 'Hikino']
+//];
+//
+////echo $arr['China'];
+//
+//foreach ($arr as $key => $value) {
+//    echo $key;
+//    foreach ($value as $item) {
+//        echo "<ul>
+//                <li>$item</li>
+//              </ul>";
+//    }
+//}
+//
+//?>
+
+<?php
+
+$id = $_GET['id'];
+
+$text = file_get_contents("document/1.txt");
+//echo nl2br($text); //властивість для переносів
+echo $text;
+
+for($i = 0; $i < count($id); )
 
 ?>
-<body style="background-image: url('<?php echo $c; ?>.jpg'); background-size: cover; color: white;">
+
+<body>
 
 <h1>php</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aspernatur commodi consectetur deleniti, eveniet
