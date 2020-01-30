9ad2abf4b269f48eaa9c70705760507f859df461
KorniichukVladimir <v1o1v1a1995>
2020-01-30 09:51:24 +0200

    file 1.1

--- index.php
+++ index.php
@@ -9,5 +9,42 @@
 </head>
 <body>
 <h1>lessons 1, time: 27:00</h1>
+
+<!--
+r - read (прочитать)
+r+ - read & write
+w - write (создать и записать)
+w+ - write & read
+a - append (добавить)
+a+ - append & read
+-->
+
+<!--read file-->
+<?php
+//
+//$f = fopen('document/1.txt', 'r');
+//
+//$length = filesize('document/1.txt');
+//
+//$str1 = fread($f, $length);
+//
+//echo nl2br($str1);
+//
+//fclose($f);
+//
+//?>
+
+
+<!--write file-->
+<?php
+$app = date('Y-m-d H:i:s') . "-||-Vladimir-||-0986209717\n";
+$f = fopen('document/apps.txt', 'a');
+fwrite($f, $app);
+fclose($f);
+?>
+
+
+
+
 </body>
 </html>
